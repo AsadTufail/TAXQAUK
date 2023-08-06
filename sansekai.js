a920a8a5c63a3a2bcf2b7066aa2c3f09beceeede
M-Umair <tariqumair017@gmail.com>
2023-08-07 00:46:49 +0500

    RND on OpenAI Keys Error and Update

--- sansekai.js
+++ sansekai.js
@@ -156,7 +156,7 @@ module.exports = sansekai = async (client, m, chatUpdate, store) => {
                             m.reply(`${response.data.choices[0].text}\n\n`)
                         } catch (err) {
                             console.log(err)
-                            m.reply('Maaf, sepertinya ada yang error')
+                            m.reply('Maaf, sepertinya aada yang error')
                         }
                         break
                     default: {
